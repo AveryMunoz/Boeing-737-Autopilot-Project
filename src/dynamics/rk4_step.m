b2241ffe2401b35492ccd364cedfa223388efdff
Avery Munoz <averymunoz10@gmail.com>
2026-02-20 14:40:12 -0500

    Clean up comments in rk4_step function
    Removed input and output documentation comments from rk4_step function.

--- src/dynamics/rk4_step.m
+++ src/dynamics/rk4_step.m
@@ -1,17 +1,6 @@
 
 function X_next = rk4_step(derivFunc, X, dt, params, elevator_cmd, throttle_cmd)
 % RK4 numerical integration step
-%
-% Inputs:
-%   derivFunc   - function handle to derivative function (flightSimulation)
-%   X           - current state vector
-%   dt          - timestep size
-%   params      - aircraft/atmosphere parameters
-%   elevator_cmd - elevator input (rad)
-%   throttle_cmd - throttle input (0–1)
-%
-% Output:
-%   X_next      - state vector at next timestep
 
     % k1 = f(t, X)
     k1 = derivFunc(0, X, params, elevator_cmd, throttle_cmd);
